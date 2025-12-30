e045163b4510ae4d3208c8ba363e9e274169ba95
Alariclg <alaric.legrais@hotmail.fr>
2025-12-30 09:00:11 +0100

    feature(config): initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,9 @@
 import type { CapacitorConfig } from "@capacitor/cli";
-import { KeyboardResize } from "@capacitor/keyboard";
 
 const config: CapacitorConfig = {
   appId: "fr.some",
   appName: "Interview",
   webDir: "dist-libmanuels",
-  server: {
-    hostname: "www.some.fr",
-  },
-  plugins: {
-    Keyboard: {
-      resize: KeyboardResize.Ionic,
-      resizeOnFullScreen: true,
-    },
-    CapacitorHttp: {
-      enabled: true,
-    },
-  },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [react()],
   base: "./",
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@page": path.resolve(__dirname, "./src/page"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@schema": path.resolve(__dirname, "./src/schema"),
+    },
+  },
   build: {
     outDir: "dist-libmanuels",
   },
