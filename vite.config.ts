d1c67e36948bf55d64e1722d0afbf71f44d2f719
Alariclg <alaric.legrais@hotmail.fr>
2026-01-05 10:45:40 +0100

    add real endpoint for demo

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://edulib.alariclg.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
